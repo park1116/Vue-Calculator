5e81c90b044e5d0dcad0dff090dad86c1eca398a
park1116 <park_1116@naver.com>
2021-08-08 15:06:33 +0900

    Create NumButton Component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,124 @@
 <template>
-    <div>
-        <app-header></app-header>
-        {{ str }}
-    </div>
+    <table>
+        <tr>
+            <td colspan="4">
+                {{ nowData }}
+            </td>
+        </tr>
+        <tr>
+            <td colspan="4">
+                {{ dataStr }}
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <num-button num=7 v-on:num-click="numClick"></num-button>
+            </td>
+            <td>
+                <num-button num=8 v-on:num-click="numClick"></num-button>
+            </td>
+            <td>
+                <num-button num=9 v-on:num-click="numClick"></num-button>
+            </td>
+            <td>
+                <button v-on:click="divClick">/</button>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <num-button num=4 v-on:num-click="numClick"></num-button>
+            </td>
+            <td>
+                <num-button num=5 v-on:num-click="numClick"></num-button>
+            </td>
+            <td>
+                <num-button num=6 v-on:num-click="numClick"></num-button>
+            </td>
+            <td>
+                <button v-on:click="mulClick">*</button>
+            </td>
+        </tr>
+        <tr>
+            <!-- <td><one-button v-bind=프롭스 속성이름:"상위컴포넌트 데이터 이름"></one-button></td> -->
+            <td>
+                <num-button num=1 v-on:num-click="numClick"></num-button>
+            </td>
+            <td>
+                <num-button num=2 v-on:num-click="numClick"></num-button>
+            </td>
+            <td>
+                <num-button num=3 v-on:num-click="numClick"></num-button>
+            </td>
+            <td>
+                <button v-on:click="subClick">-</button>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <button v-on:click="allCancel">C</button>
+            </td>
+            <td>
+                <num-button num=0 v-on:num-click="numClick"></num-button>
+            </td>
+            <td>
+                <button v-on:click="oneCancel">◁</button>
+            </td>
+            <td>
+                <button v-on:click="addClick">+</button>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="4">
+                <button v-on:click="numCalculate">
+                    =
+                </button>
+            </td>
+        </tr>
+    </table>
 </template>
 
 <script>
-import AppHeader from './components/AppHeader.vue'
+import NumButton from './components/NumButton.vue'
 
 export default {
     data: function(){
         return{
-            str: 'hi'
+            dataStr: '',
+            nowData: 0,
+            fromData: 0
         }
     },
     components: {
-        'app-header': AppHeader
+        'num-button': NumButton
+    },
+    methods: {
+        // 현재 numClick이 문자로 인식되는 문제가 있음---------------------------------------------------------
+        numClick: function(numClick) {
+            this.nowData = numClick;
+            this.dataStr += numClick;
+        },
+        numCalculate: function() {
+            this.nowData = this.fromData + this.dataStr;
+        },
+        oneCancel: function() {
+        },
+        allCancel: function() {
+            this.dataStr = '';
+            this.fromData = 0;
+            this.nowData = 0;
+        },
+        divClick: function() {
+
+        },
+        mulClick: function() {
+            
+        },
+        subClick: function() {
+            
+        },
+        addClick: function() {
+            
+        }
     }
 }
 </script>
---
+++ src/components/NumButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <button v-on:click="numClick"> {{ num }} </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        num: Number
+    },
+    methods: {
+        numClick: function() {
+            this.$emit('num-click', this.num)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
