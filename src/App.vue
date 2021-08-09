315b0b4a73d1cc405ed694efce72a3113ca66b8f
park1116 <park_1116@naver.com>
2021-08-09 22:44:22 +0900

    First Calculate Success

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,80 @@
 <template>
-    <table>
-        <tr>
-            <td colspan="4">
-                {{ nowData }}
-            </td>
-        </tr>
-        <tr>
-            <td colspan="4">
-                {{ dataStr }}
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <num-button num=7 v-on:num-click="numClick"></num-button>
-            </td>
-            <td>
-                <num-button num=8 v-on:num-click="numClick"></num-button>
-            </td>
-            <td>
-                <num-button num=9 v-on:num-click="numClick"></num-button>
-            </td>
-            <td>
-                <button v-on:click="divClick">/</button>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <num-button num=4 v-on:num-click="numClick"></num-button>
-            </td>
-            <td>
-                <num-button num=5 v-on:num-click="numClick"></num-button>
-            </td>
-            <td>
-                <num-button num=6 v-on:num-click="numClick"></num-button>
-            </td>
-            <td>
-                <button v-on:click="mulClick">*</button>
-            </td>
-        </tr>
-        <tr>
-            <!-- <td><one-button v-bind=프롭스 속성이름:"상위컴포넌트 데이터 이름"></one-button></td> -->
-            <td>
-                <num-button num=1 v-on:num-click="numClick"></num-button>
-            </td>
-            <td>
-                <num-button num=2 v-on:num-click="numClick"></num-button>
-            </td>
-            <td>
-                <num-button num=3 v-on:num-click="numClick"></num-button>
-            </td>
-            <td>
-                <button v-on:click="subClick">-</button>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <button v-on:click="allCancel">C</button>
-            </td>
-            <td>
-                <num-button num=0 v-on:num-click="numClick"></num-button>
-            </td>
-            <td>
-                <button v-on:click="oneCancel">◁</button>
-            </td>
-            <td>
-                <button v-on:click="addClick">+</button>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="4">
-                <button v-on:click="numCalculate">
-                    =
-                </button>
-            </td>
-        </tr>
-    </table>
+    <div>
+        <div>
+            <p>
+                {{ fromData }} {{ exp }} {{ dataStr }}
+            </p>
+            <p v-if="nowData != 0">
+                = {{ nowData }}
+            </p>
+        </div>
+        <table>
+            <tr>
+                <td>
+                    <num-button num=7 v-on:num-click="numClick"></num-button>
+                </td>
+                <td>
+                    <num-button num=8 v-on:num-click="numClick"></num-button>
+                </td>
+                <td>
+                    <num-button num=9 v-on:num-click="numClick"></num-button>
+                </td>
+                <td>
+                    <button v-on:click="divClick">/</button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <num-button num=4 v-on:num-click="numClick"></num-button>
+                </td>
+                <td>
+                    <num-button num=5 v-on:num-click="numClick"></num-button>
+                </td>
+                <td>
+                    <num-button num=6 v-on:num-click="numClick"></num-button>
+                </td>
+                <td>
+                    <button v-on:click="mulClick">*</button>
+                </td>
+            </tr>
+            <tr>
+                <!-- <td><one-button v-bind=프롭스 속성이름:"상위컴포넌트 데이터 이름"></one-button></td> -->
+                <td>
+                    <num-button num=1 v-on:num-click="numClick"></num-button>
+                </td>
+                <td>
+                    <num-button num=2 v-on:num-click="numClick"></num-button>
+                </td>
+                <td>
+                    <num-button num=3 v-on:num-click="numClick"></num-button>
+                </td>
+                <td>
+                    <button v-on:click="subClick">-</button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <button v-on:click="allCancel">C</button>
+                </td>
+                <td>
+                    <num-button num=0 v-on:num-click="numClick"></num-button>
+                </td>
+                <td>
+                    <button v-on:click="oneCancel">◁</button>
+                </td>
+                <td>
+                    <button v-on:click="addClick">+</button>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4">
+                    <button v-on:click="numCalculate">
+                        =
+                    </button>
+                </td>
+            </tr>
+        </table>
+    </div>
 </template>
 
 <script>
@@ -85,7 +85,8 @@ export default {
         return{
             dataStr: '',
             nowData: 0,
-            fromData: 0
+            fromData: 0,
+            exp: ''
         }
     },
     components: {
@@ -94,11 +95,17 @@ export default {
     methods: {
         // 현재 numClick이 문자로 인식되는 문제가 있음---------------------------------------------------------
         numClick: function(numClick) {
-            this.nowData = numClick;
             this.dataStr += numClick;
         },
         numCalculate: function() {
-            this.nowData = this.fromData + this.dataStr;
+            if(this.exp == '+')
+               this.nowData = this.fromData + parseInt(this.dataStr);
+            else if(this.exp == '-')
+               this.nowData = this.fromData - parseInt(this.dataStr);
+            else if(this.exp == '*')
+               this.nowData = this.fromData * parseInt(this.dataStr);
+            else if(this.exp == '/')
+               this.nowData = this.fromData / parseInt(this.dataStr);
         },
         oneCancel: function() {
         },
@@ -108,16 +115,24 @@ export default {
             this.nowData = 0;
         },
         divClick: function() {
-
+            this.exp = '/';
+            this.fromData = parseInt(this.dataStr); 
+            this.dataStr = '';
         },
         mulClick: function() {
-            
+            this.exp = '*';
+            this.fromData = parseInt(this.dataStr);
+            this.dataStr = '';
         },
         subClick: function() {
-            
+            this.exp = '-';
+            this.fromData = parseInt(this.dataStr);
+            this.dataStr = '';
         },
         addClick: function() {
-            
+            this.exp = '+';
+            this.fromData = parseInt(this.dataStr);
+            this.dataStr = '';
         }
     }
 }
